e5a0e762c93f0cd4625c10110063b630c5f26ea4
lirugo <roostik21@gmail.com>
2018-12-26 17:33:03 +0200

    Removed all API, Models, Migrations, Tests..

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,7 +11,5 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-         $this->call(WorkPositionSeeder::class);
-         $this->call(UserSeeder::class);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -5,10 +5,3 @@
 | API Routes
 |--------------------------------------------------------------------------
 */
-
-//Users
-Route::get('users', 'Api\User\UserController@index');
-Route::get('users/{user}', 'Api\User\UserController@show');
-Route::post('users', 'Api\User\UserController@store');
-Route::put('users/{user}', 'Api\User\UserController@update');
-Route::delete('users/{user}', 'Api\User\UserController@delete');
